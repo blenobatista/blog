47f52dc2c57e98defa3db9d78ece5af3f7d5ac9f
blenobatista <36760152+blenobatista@users.noreply.github.com>
2018-02-26 14:03:40 +0100

    final de la clase

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
     
 <div class="pie">
     <div class="contenidopie">
-        <a href= "https://www.jccm.es/" target= "blank"><img class="junta" src="imagenes/comunes/junta.jpg" alt="logo de la jccm" title="logo de la junta"/></a>
-        <a href= "https://www.ajeclm.com/programa-tic" target= "blank"><img class="programatic" src="imagenes/comunes/programatic.jpg" alt="programatic" title="programatic"/></a>
-          <a href= "https://www.ajeclm.com/" target= "blank"><img class="aje" src="imagenes/comunes/aje.jpg" alt="logo de aje" title="logo de aje"/></a>
-          <a href= "ec.europa.eu/esf/home.jsp?langId=es" target= "blank"><img class="europeo" src="imagenes/comunes/europeo.jpg" alt="logo del fondo europeo" title="logo del fondo europeo"/></a>
+        <a href= "http://www.jccm.es/" target= "blank"><img class="junta" src="imagenes/comunes/junta.jpg" alt="logo de la jccm" title="logo de la junta"/></a>
+        <a href= "http://www.ajeclm.com/programa-tic" target= "blank"><img class="programatic" src="imagenes/comunes/programatic.jpg" alt="programatic" title="programatic"/></a>
+          <a href= "http://www.ajeclm.com/" target= "blank"><img class="aje" src="imagenes/comunes/aje.jpg" alt="logo de aje" title="logo de aje"/></a>
+          <a href= "http://ec.europa.eu/esf/home.jsp?langId=es" target= "blank"><img class="europeo" src="imagenes/comunes/europeo.jpg" alt="logo del fondo europeo" title="logo del fondo europeo"/></a>
     </div>
 </div>
 </div>
