422339456d7b4861a91d61d56311d6cf3a0f4009
blenobatista <36760152+blenobatista@users.noreply.github.com>
2018-02-26 13:57:02 +0100

    final de la clase del lunes 26

--- index.php
+++ index.php
@@ -8,23 +8,27 @@
 
 <body>
 <div class="cabecera">
-    <div class="contenidocabecera"
-       
+    <div class="contenidocabecera">
+       <a href= "http://www.ajeclm.com/" target= "blank" class="logo"></a>
+       <a href= "index.php"><h1>Blog del programa TIC</h1></a>
     </div>
 
 </div>
 <div class="fondo">
-    <div class="contenido"
+    <div class="contenido">
         
     </div>
     
-</div>
 <div class="pie">
-    <div class="contenidopie"
-        
+    <div class="contenidopie">
+        <a href= "https://www.jccm.es/" target= "blank"><img class="junta" src="imagenes/comunes/junta.jpg" alt="logo de la jccm" title="logo de la junta"/></a>
+        <a href= "https://www.ajeclm.com/programa-tic" target= "blank"><img class="programatic" src="imagenes/comunes/programatic.jpg" alt="programatic" title="programatic"/></a>
+          <a href= "https://www.ajeclm.com/" target= "blank"><img class="aje" src="imagenes/comunes/aje.jpg" alt="logo de aje" title="logo de aje"/></a>
+          <a href= "https://ec.europa.euesf/home.jsp?langId=es" target= "blank"><img class="europeo" src="imagenes/comunes/europeo.jpg" alt="logo del fondo europeo" title="logo del fondo europeo"/></a>
     </div>
-    
 </div>
+</div>
+
 </body>
 </html>
 
